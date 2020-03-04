332715833c03f4b291d4695816e3f08a90e18ed7
Joey Nevarez <josephnevarez12@gmail.com>
2020-03-04 14:31:32 -0800

    scheme model file completed

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -1,23 +1,38 @@
 const db = require('../data/config')
 
 function find() {
-
+    return db("schemes")
 }
 
 function findById(id) {
-
+    return db("schemes")
+        .where({id: id})
+        .first()
 }
 
-function add(scheme){
+function findSteps(id){
+    return db("schemes as s")
+        .where({scheme_id: id})
+        .join("steps as st", "s.id", "st.scheme_id")
+        .select("s.id", "s.scheme_name", "st.step_number", "st.instructions")
+        .orderBy("st.step_number", "desc")
+}
 
+function add(schemeData){
+    return db("schemes").insert(schemeData)
+}
+
+function addStep(stepData, id) {
+    db("steps").insert(stepData)
+    return db("steps").where()
 }
 
 function update(changes, id){
-
+    return db("schemes").where({id: id}).update(changes)
 }
 
 function remove(id){
-
+    return db("schemes").where({id: id}).del()
 }
 
 module.exports = {
@@ -25,5 +40,7 @@ module.exports = {
     findById,
     add,
     update,
-    remove
+    remove,
+    findSteps, 
+    addStep
 }
